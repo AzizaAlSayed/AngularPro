a733670895a0ee7f7f3bf2f35ee0aa5381ec1623
lara <36706867+laraSaidSayedAhmad@users.noreply.github.com>
2020-10-23 21:44:10 +0300

    Add project files.

---
+++ Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AngularPro.Models
+{
+    public partial class Tasks
+    {
+        public int TaskId { get; set; }
+        public string Subject { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public bool Completed { get; set; }
+        public string Importance { get; set; }
+    }
+}
---
+++ Models/masterContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularPro.Models
+{
+    public partial class masterContext : DbContext
+    {
+        public masterContext()
+        {
+        }
+
+        public masterContext(DbContextOptions<masterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<TaskCategories> TaskCategories { get; set; }
+        public virtual DbSet<Tasks> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-2U9R9985;Database=master;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId)
+                    .HasName("PK__Categori__19093A0BB37AC147");
+
+                entity.Property(e => e.CategoryName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TaskCategories>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.HasOne(d => d.Category)
+                    .WithMany()
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__TaskCateg__Categ__15702A09");
+
+                entity.HasOne(d => d.Task)
+                    .WithMany()
+                    .HasForeignKey(d => d.TaskId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__TaskCateg__TaskI__16644E42");
+            });
+
+            modelBuilder.Entity<Tasks>(entity =>
+            {
+                entity.HasKey(e => e.TaskId)
+                    .HasName("PK__Tasks__7C6949B160491208");
+
+                entity.Property(e => e.CompletedDate).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.CreateTime).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.DueDate).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.Importance)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StartDate).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.Subject)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
